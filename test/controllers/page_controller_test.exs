35626ccd4c4a74ffad9c350a435f6fa4c65f02c3
Mark Connell <mark@markconnell.co.uk>
2015-08-07 11:25:07 +0100

    mess with the generated index and layout templates

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Fakebook.PageControllerTest do
 
   test "GET /" do
     conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "This is the landing page of the fakebook. Wooo!"
   end
 end
